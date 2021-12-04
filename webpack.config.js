41ebf4f357d7382013c387ab5f04b926d717f561
Richard Nagy <nagy.richard.antal@gmail.com>
2021-12-04 15:46:40 +0100

    Added uglifyjs webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
 const path = require('path')
-
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.js', '.ts' ],
-  },
-  output: {
-    filename: 'browser-fingerprint.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+ 
+module.exports =
+{
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve:
+    {
+        extensions: [ '.js', '.ts' ],
+    },
+    output:
+    {
+        filename: 'browser-fingerprint.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization:
+    {
+        minimizer: [new UglifyJsPlugin()],
+    },
 }
